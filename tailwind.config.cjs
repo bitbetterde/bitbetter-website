c785eafe57acdd90d1cf60312a2b3256719d654b
Moritz Stückler <moritz@bitbetter.de>
2023-05-25 17:38:27 +0200

    feat: astro setup

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        "bb-grey": "#f3f3f3",
+      },
+    },
+  },
+  plugins: [],
+};
