a491940a4a2ba9f0fd7804d4484954969be4cbbb
Fabian Schmidt <fabian@ds-fs.de>
2023-05-25 17:58:20 +0200

    install prettier & add pre commit hook

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,9 +1,9 @@
 const HeroSection = () => {
   return (
-    <div className="h-48 bg-bb-grey">
-      <h1 className="text-red-500">Hello</h1>
+    <div className='h-48 bg-bb-grey'>
+      <h1 className='text-red-500'>Hello</h1>
     </div>
-  );
-};
+  )
+}
 
-export default HeroSection;
+export default HeroSection
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
       colors: {
-        "bb-grey": "#f3f3f3",
+        'bb-grey': {
+          200: '#f3f3f3',
+          400: '#EAEAEA',
+          600: '#2B2B2B',
+        },
+        'bb-blue': {
+          200: '#323F4B',
+          400: '#182B38',
+        },
       },
     },
   },
   plugins: [],
-};
+}
