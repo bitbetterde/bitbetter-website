027931f610f8e657e72bd0fd535dbd932a6eedef
Fabian <fabian@ds-fs.de>
2023-06-09 13:34:56 +0200

    init footer (#4)

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,16 +4,17 @@ interface Props {
   caption: string
   className?: string
   onClick?: () => void
+  dark?: boolean
 }
 
-const Button = ({ caption, onClick, className }: Props) => {
+const Button = ({ caption, onClick, className, dark = false }: Props) => {
   return (
     <button
       type='button'
       onClick={onClick}
-      className={`bg-black flex gap-2 py-4 px-5 rounded-full items-center font-medium text-white ${
-        className || ''
-      }`}
+      className={`flex gap-2 py-4 px-5 rounded-full items-center font-medium ${
+        dark ? 'bg-black text-white' : 'bg-white text-bb-grey-500'
+      } ${className || ''}`}
     >
       {caption}
       <ArrowRightIcon className='h-6 w-6' />
