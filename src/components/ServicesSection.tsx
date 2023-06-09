389b9636595d558187bc3e16e716d55b3054c98d
Fabian Schmidt <fabian@ds-fs.de>
2023-06-09 11:31:52 +0200

    add logos & example content to services

--- src/components/ServicesSection.tsx
+++ src/components/ServicesSection.tsx
@@ -4,6 +4,10 @@ import ServiceSection from '@components/ServiceSection'
 import ServiceSectionDivider from '@components/ServiceSectionDivider'
 import { ReactComponent as GlobeIcon } from '@phosphor-icons/core/regular/globe.svg'
 import { ReactComponent as LightningIcon } from '@phosphor-icons/core/regular/lightning.svg'
+import { ReactComponent as TerminalWindowIcon } from '@phosphor-icons/core/regular/terminal-window.svg'
+import { ReactComponent as SidebarIcon } from '@phosphor-icons/core/regular/sidebar.svg'
+import { ReactComponent as ChatsIcon } from '@phosphor-icons/core/regular/chats.svg'
+import { ReactComponent as OSIIcon } from '@assets/osi.svg'
 
 interface Props {}
 
@@ -39,9 +43,9 @@ const ServicesSection: React.FC<Props> = () => {
           icon={<LightningIcon />}
         />
         <ServiceSection
-          title={'Automatisierung'}
+          title={'Webanwendungen'}
           text={
-            'Wir verknüpfen und integrieren Ihre bestehenden Dienste und sorgen für Interoperabilität und Optimierung.'
+            'Wir kennen den Markt und helfen dabei, die ideale Softwarelösung für Ihren Use Case zu finden.'
           }
           icon={<GlobeIcon />}
         />
@@ -53,12 +57,14 @@ const ServicesSection: React.FC<Props> = () => {
             'Wir konstruieren performante Server-Anwendungen mit Hilfe von APIs, Microservices und Datenbanken.'
           }
           dark={true}
+          icon={<TerminalWindowIcon />}
         />
         <ServiceSection
-          title={'Automatisierung'}
+          title={'Open Source'}
           text={
-            'Wir verknüpfen und integrieren Ihre bestehenden Dienste und sorgen für Interoperabilität und Optimierung.'
+            'Wir helfen Ihnen, Software, Methoden & Prozesse aus der Open-Source-Welt zu verstehen und einzusetzen.'
           }
+          icon={<OSIIcon />}
         />
         <ServiceSectionDivider className={'bg-black'} />
         <ServiceSectionDivider className={'bg-bb-grey-200'} />
@@ -68,12 +74,12 @@ const ServicesSection: React.FC<Props> = () => {
             'Wir implementieren hochwertige und interaktive Frontends mit Hilfe modernster Web-Technologien.'
           }
           dark={true}
+          icon={<SidebarIcon />}
         />
         <ServiceSection
-          title={'Automatisierung'}
-          text={
-            'Wir verknüpfen und integrieren Ihre bestehenden Dienste und sorgen für Interoperabilität und Optimierung.'
-          }
+          title={'Messaging'}
+          text={'Wir helfen Ihnen dabei, die ideale Messenger-Lösung für Ihr Szenario zu finden.'}
+          icon={<ChatsIcon />}
         />
         <div className={'h-8 bg-black'} />
         <div className={'h-8 bg-bb-grey-200'} />
