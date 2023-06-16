d1fc22306a03bc72d583c1a389dec3f939643392
Fabian Schmidt <fabian@ds-fs.de>
2023-06-16 11:20:00 +0200

    add github actions for build

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://bitbetter.de',
   integrations: [react(), tailwind({ config: { applyBaseStyles: false } })],
   vite: {
     plugins: [svgr()],
