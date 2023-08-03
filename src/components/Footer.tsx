fed47331ce73719b8ee55896211bbfe8c3235a40
Moritz Stückler <moritz.stueckler@gmail.com>
2023-08-03 10:11:54 +0200

    feat: add website carbon badge (#17)
    * feat: add website carbon badge
    
    * fix: line break in website title
    
    * chore: update astro
    
    * doc: update readme
    
    * change to black&white theme
    
    ---------
    
    Co-authored-by: Fabian Schmidt <fabian@ds-fs.de>

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -12,29 +12,33 @@ interface Props {
 const Footer: React.FC<Props> = ({ children }: Props) => {
   return (
     <footer
-      className={'flex flex-col md:flex-row pt-12 md:pt-20 px-5 md:px-28 pb-14 gap-y-5 relative'}
+      className={'flex flex-col md:flex-row pt-12 md:pt-20 px-5 lg:px-28 pb-14 gap-y-12 relative'}
     >
       <div className={'flex-[2] flex gap-8 flex-col justify-between md:pt-0.5 z-10'}>
         <div>
           <BbLogo className={'h-8 md:h-10'} />
         </div>
-        <div className='flex gap-2'>
-          <a
-            href='https://github.com/bitbetterde/'
-            target='_blank'
-            className='text-black hover:text-opacity-75'
-            aria-label='bitbetter auf GitHub'
-          >
-            <GithubIcon className='p-2 h-10 w-10' />
-          </a>
-          <a
-            href='https://www.linkedin.com/company/bitbetter/'
-            target='_blank'
-            className='text-black hover:text-opacity-75'
-            aria-label='bitbetter auf LinkedIn'
-          >
-            <LinkedInIcon className='p-2 h-10 w-10' />
-          </a>
+
+        <div className='flex flex-col gap-6 items-start'>
+          <div className='flex gap-2'>
+            <a
+              href='https://github.com/bitbetterde/'
+              target='_blank'
+              className='text-black hover:text-opacity-75'
+              aria-label='bitbetter auf GitHub'
+            >
+              <GithubIcon className='p-2 h-10 w-10' />
+            </a>
+            <a
+              href='https://www.linkedin.com/company/bitbetter/'
+              target='_blank'
+              className='text-black hover:text-opacity-75'
+              aria-label='bitbetter auf LinkedIn'
+            >
+              <LinkedInIcon className='p-2 h-10 w-10' />
+            </a>
+          </div>
+          {children}
         </div>
       </div>
       <div className={'flex-[3] z-10'}>
@@ -47,7 +51,30 @@ const Footer: React.FC<Props> = ({ children }: Props) => {
             'flex flex-col md:flex-row justify-between text-base md:text-lg leading-[1.4] mt-[11.5rem] md:mt-[17.875rem]'
           }
         >
-          {children}
+          <address className={'not-italic mt-8 md:mt-0'}>
+            <a className='underline' href='mailto:info@bitbetter.de'>
+              info@bitbetter.de
+            </a>
+            <br />
+            <br />
+            bitbetter GmbH <br />
+            Desenißstr. 37 <br />
+            22083 Hamburg <br />
+            <br />
+            Registergericht: <br />
+            Amtsgericht Hamburg (HRB 177719) <br />
+          </address>
+          <div>
+            <br />
+            <br />
+            Vertreten durch die Geschäftsführer: <br />
+            Moritz Stückler
+            <br />
+            Fabian Schmidt <br />
+            <br />
+            UStIdNr: DE358143925 <br />
+            © 2023 bitbetter GmbH <br />
+          </div>
         </div>
       </div>
       <BbBracket className='text-white scale-90 absolute w-72 -bottom-16 left-0 hidden md:block' />
