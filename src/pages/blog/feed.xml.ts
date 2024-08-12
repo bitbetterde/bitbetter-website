a6bf2a89634d6197ca39039bd7a37384c35e3288
Moritz Stückler <moritz@bitbetter.de>
2024-08-12 23:36:26 +0200

    fix: rss feed

--- src/pages/blog/feed.xml.ts
+++ src/pages/blog/feed.xml.ts
@@ -1,17 +1,18 @@
 import rss from '@astrojs/rss'
+import type { APIContext } from 'astro'
 import { getCollection } from 'astro:content'
 
-export async function GET(context) {
-  const content = (await getCollection('blog')).sort((a, b) => b.data.date - a.data.date)
+export async function GET(context: APIContext) {
+  const content = (await getCollection('blog')).sort((a, b) => (a.data.date > b.data.date ? -1 : 1))
   return rss({
     title: 'bitbetter blog',
     description:
       'Wir sind zwei Software-Entwickler aus Hamburg und lösen Probleme im Bereich der Digitalisierung: Making the world a bit better.',
-    site: context.site,
+    site: context?.site?.href || 'http://bitbetter.de',
     items: content.map((post) => ({
       title: post.data.title,
       pubDate: post.data.date,
-      description: post.data.description,
+      description: post.data.teaser,
       link: `/blog/${post.slug}/`,
     })),
     customData: `<language>de-DE</language>`,
