f204e33721fe2d0a5ba1dd2b4b5c1e85779e92a1
Moritz Stückler <moritz.stueckler@gmail.com>
2024-08-14 14:09:57 +0200

    Feat/customers section (#24)
    * chore: dependency updates
    
    * feat: add customer logos, wip
    
    * feat: add cosh logo
    
    * fix: customer links
    
    * chore: update dependencies
    
    * fix: headline size
    
    * fix: rss feed
    
    * fix: add ow law logo

---
+++ src/components/CustomersSection.tsx
@@ -0,0 +1,48 @@
+import { SectionMark } from '@components'
+import HsuLogo from '@assets/customers/hsu.svg?react'
+import HiwwLogo from '@assets/customers/hiww.svg?react'
+import FchhLogo from '@assets/customers/fchh.svg?react'
+import OwLogo from '@assets/customers/ow.svg?react'
+import SchleyLogo from '@assets/customers/schley.svg?react'
+import CoshLogo from '@assets/customers/cosh.svg?react'
+
+const CustomersSection: React.FC = () => {
+  const linkClasses = 'block flex items-center justify-center p-4 customer-logo'
+  const logoClasses = 'h-16 w-auto fill-slate-800'
+  return (
+    <>
+      <div
+        className={'xl:col-span-3 xl:col-start-2 pt-16 pb-0 xl:py-20 px-5 2xl:px-0'}
+        id='customers'
+      >
+        <SectionMark
+          title={'Kunden & Partner'}
+          classNameLine={'bg-black/75'}
+          classNameText={'opacity-75'}
+        />
+      </div>
+      <div className='xl:col-span-8 grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 py-16 gap-4 xl:gap-8'>
+        <a href='https://cosh.de/' target='_blank' className={linkClasses}>
+          <CoshLogo className={logoClasses} />
+        </a>
+        <a href='https://www.hiww.de/' target='_blank' className={linkClasses}>
+          <HiwwLogo className={logoClasses} />
+        </a>
+        <a href='https://www.ios-schley.de/' target='_blank' className={linkClasses}>
+          <SchleyLogo className={logoClasses} />
+        </a>
+        <a href='https://www.hsu-hh.de/' target='_blank' className={linkClasses}>
+          <HsuLogo className={logoClasses} />
+        </a>
+        <a href='https://www.fabcity.hamburg/' target='_blank' className={linkClasses}>
+          <FchhLogo className={logoClasses} />
+        </a>
+        <a href='https://www.owlaw.de/' target='_blank' className={linkClasses}>
+          <OwLogo className={logoClasses} />
+        </a>
+      </div>
+    </>
+  )
+}
+
+export default CustomersSection
