bd1fc2f6d7b3dae7f3871dc3666e71b9f21037ee
Moritz Stückler <moritz@bitbetter.de>
2024-10-04 16:42:14 +0200

    fix: add aria labels to customer logos

--- src/components/CustomersSection.tsx
+++ src/components/CustomersSection.tsx
@@ -22,22 +22,52 @@ const CustomersSection: React.FC = () => {
         />
       </div>
       <div className='xl:col-span-8 grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 py-16 gap-4 xl:gap-8'>
-        <a href='https://cosh.de/' target='_blank' className={linkClasses}>
+        <a
+          href='https://cosh.de/'
+          target='_blank'
+          aria-label='CosH Consulting GmbH'
+          className={linkClasses}
+        >
           <CoshLogo className={logoClasses} />
         </a>
-        <a href='https://www.hiww.de/' target='_blank' className={linkClasses}>
+        <a
+          href='https://www.hiww.de/'
+          target='_blank'
+          aria-label='Hamburger Institut für Wertschöpfungssystematik und Wissensmanagement UG'
+          className={linkClasses}
+        >
           <HiwwLogo className={logoClasses} />
         </a>
-        <a href='https://www.ios-schley.de/' target='_blank' className={linkClasses}>
+        <a
+          href='https://www.ios-schley.de/'
+          target='_blank'
+          aria-label='IOS Institut für Organisationsentwicklung und Systemberatung Schley & Partner GmbH'
+          className={linkClasses}
+        >
           <SchleyLogo className={logoClasses} />
         </a>
-        <a href='https://www.hsu-hh.de/' target='_blank' className={linkClasses}>
+        <a
+          href='https://www.hsu-hh.de/'
+          target='_blank'
+          aria-label='Helmut-Schmidt-Universität/Universität der Bundeswehr Hamburg'
+          className={linkClasses}
+        >
           <HsuLogo className={logoClasses} />
         </a>
-        <a href='https://www.fabcity.hamburg/' target='_blank' className={linkClasses}>
+        <a
+          href='https://www.fabcity.hamburg/'
+          target='_blank'
+          aria-label='Fab City Hamburg e.V.'
+          className={linkClasses}
+        >
           <FchhLogo className={logoClasses} />
         </a>
-        <a href='https://www.owlaw.de/' target='_blank' className={linkClasses}>
+        <a
+          href='https://www.owlaw.de/'
+          target='_blank'
+          aria-label='O&W Rechtsanwaltsgesellschaft mbH'
+          className={linkClasses}
+        >
           <OwLogo className={logoClasses} />
         </a>
       </div>
